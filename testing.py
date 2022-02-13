df10f763606a18d6c29ccd1438af3cc6f997a63a
Alex St. Clair <awstclair@gmail.com>
2022-02-13 16:30:06 -0500

    first couple videos

---
+++ testing.py
@@ -0,0 +1,14 @@
+# ints, floats, booleans, strings
+# name = value
+
+number_of_friends = 10
+number_of_friends = 15
+
+# print(type(number_of_friends))
+paycheck = 789.53
+# print(type(paycheck))
+# print(paycheck)
+isDoorOpen = False
+
+print(isDoorOpen)
+print(type(isDoorOpen))
